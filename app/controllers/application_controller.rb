99372be9d6cfcce834039ebcd36a5d64dd42d548
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-04-07 18:00:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,52 +1,53 @@
 require_relative '../../config/engironment'
 
-class ApplicationController < Sinatra::Base 
-  configure do 
+class ApplicationController < Sinatra::Base
+  
+  configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-end 
+end
 
-  get 'posts/new' do 
+  get 'posts/new' do
 
-    erb :new 
-  end 
+    erb :new
+  end
 
-  post '/posts' do 
+  post '/posts' do
     @post = Post.create(params)
     redirect to '/posts'
-  end 
+  end
 
-  get 'posts' do 
-    @posts = Post.all 
+  get 'posts' do
+    @posts = Post.all
 
-    erb :index 
-  end 
+    erb :index
+  end
 
-  get '/posts/:id' do 
+  get '/posts/:id' do
     @post = Post.find_by_id(params[:id])
 
-    erb :show 
-  end 
+    erb :show
+  end
 
-  get 'posts/:id/edit' do 
+  get 'posts/:id/edit' do
     @post = Post.find_by_id(params[:id])
 
-    erb :edit 
-  end 
+    erb :edit
+  end
 
-  patch '/posts/:id' do 
+  patch '/posts/:id' do
     @post = Post.find_by_id(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
     @post.save
 
-    erb :show 
-  end 
+    erb :show
+  end
 
-  delete '/posts/:id/delete' do 
+  delete '/posts/:id/delete' do
     @post = Post.find_by_id(params[:id])
-    @post.delete 
+    @post.delete
 
-    erb :delete 
-  end 
-end#class 
+    erb :delete
+  end
+end#class
